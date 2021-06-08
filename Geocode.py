413f10fdc2ffed321fd27f4e20df9bef882e025e
Pavel <49796077+QlaVs@users.noreply.github.com>
2021-06-08 16:00:38 +0300

    Add files via upload

---
+++ Geocode.py
@@ -0,0 +1,61 @@
+"""
+Создаёт список координат, комбинируя значения одного места из главного списка, потом ищет по запросу
+Пример:
+Россия, Москва, Красная площадь
+>> Россия >> Москва >> Красная площадь >> Россия, Красная площадь >> Москва, Красная площадь >> Россия, Москва
+И так далее
+"""
+
+import json
+import pandas
+import Info
+import itertools
+import requests
+
+
+def geocoding():
+
+    places = []
+    quantity = 100
+
+    url = Info.search_url
+
+    data = pandas.read_json('main_data.json')
+    for i in range(len(data.index)):
+        names = data.loc[i]['display_name'].split(',')
+        for j in range(1, len(names) + 1):
+            for name in itertools.combinations(names, j):
+                if quantity >= 0:
+
+                    try:
+                        format_name = ','.join(name).replace(' ', '+')
+                        if format_name[0] == "+":
+                            format_name = format_name[1:]
+                        print(quantity, format_name.replace('+', ' '))
+                        r = requests.get(f"{url}?q={format_name}&format=jsonv2")
+                        json_data = r.json()
+                        for z in json_data:
+                            places.append({"name": format_name.replace('+', ' '),
+                                           'display_name': z['display_name'],
+                                           'lat': float(z['lat']),
+                                           'lon': float(z['lon'])})
+
+                        # Выглядит лучше, но усложняет тест
+                        # places.append({
+                        #     "name": f"{name_str}",
+                        #     "info": [{'display_name': z['display_name'],
+                        #               'lat': float(z['lat']),
+                        #               'lon': float(z['lon'])} for z in json_data]
+                        # })
+
+                        quantity -= 1
+
+                    except:
+                        pass
+
+    with open("geocode.json", "w") as outfile:
+        json.dump(places, outfile)
+
+
+if __name__ == '__main__':
+    geocoding()
---
+++ Test_ddt.py
@@ -0,0 +1,31 @@
+import unittest
+import os.path
+import Info
+from ddt import ddt, file_data
+
+
+# Используем названия из geocode.json
+# Проверяем, сходятся ли координаты из geocode.json с полученными только что с сайта при поиске по названию
+@ddt
+class Geocoding(unittest.TestCase):
+    @file_data(os.path.join("geocode.json"))
+    def test_coords(self, name, lat, lon, display_name):
+        self.assertEqual(Info.get_coords(name), (float(lat), float(lon)))
+
+    # То же самое, но округляем координаты для менее точного сравнения (условная погрешность)
+    @file_data(os.path.join("geocode.json"))
+    def test_round_coords(self, name, lat, lon, display_name):
+        self.assertEqual(Info.get_round_coords(name), (round(float(lat), 3), round(float(lon), 3)))
+
+
+# Используем координаты из main_data.json
+# Проверяем, сходятся ли названия из main_data.json с полученными только что с сайта при поиске по координатам
+@ddt
+class RevGeocoding(unittest.TestCase):
+    @file_data(os.path.join("main_data.json"))
+    def test_reverse(self, display_name, lat, lon,  address):
+        self.assertEqual(str(display_name), Info.get_display_name(lat, lon))
+
+
+if __name__ == '__main__':
+    unittest.main()
